9ed98add02739f6f4ba1dc784ab48234e0834f56
Simone <2827442l@student.gla.ac.uk>
2023-03-04 23:56:00 +0000

    quick but ugly fix for rating-caused bugs for anonymous users

--- rmr/views.py
+++ rmr/views.py
@@ -212,9 +212,12 @@ def show_recipe(request, category_name_slug, recipe_title_slug):
     context_dict = {}
     try:
         recipe = Recipe.objects.get(slug=recipe_title_slug)
-        rating = Rating.objects.filter(user=request.user, recipe=recipe).first()
         context_dict['recipe'] = recipe
-        context_dict['rating'] = rating
+        if request.user.is_anonymous:
+            context_dict['rating'] = None
+        else:
+            rating = Rating.objects.filter(user=request.user, recipe=recipe).first()
+            context_dict['rating'] = rating
 
         if request.method == 'POST':
             form = RatingForm(request.POST)
