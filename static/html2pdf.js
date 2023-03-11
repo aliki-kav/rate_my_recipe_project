6f80477bb86be3d7838008fb9782febd3abb56e7
Aliki Kavoussanou-Kavoussanaki <2554319k@student.gla.ac.uk>
2023-03-11 17:30:08 +0000

    download pdf fixed I hope

--- static/html2pdf.js
+++
@@ -1,3 +0,0 @@
-let htmlPDF = document.getElementById("html2pdf");
-let exportPDF = document.getElementById("pdf");
-exportPDF.onclick = (e) => html2pdf(htmlPDF);
---
+++ static/pdfconverter.js
@@ -0,0 +1,7 @@
+function myFunction(){
+    let htmlPDF = document.getElementById("html2pdf");
+    html2pdf(htmlPDF);
+}
+
+
+        
