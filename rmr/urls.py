142aa1d5088cdd813a32cd538bf346c86c4cc52f
Anthony <2689407e@student.gla.ac.uk>
2023-03-18 20:14:56 +0000

    pdf change

--- rmr/urls.py
+++ rmr/urls.py
@@ -12,13 +12,7 @@ urlpatterns = [
     path('category/<category_name_slug>/add_page/', views.add_page, name='add_page'),
     path('register/', views.register, name='register'),
     path('login/', views.user_login, name='login'),
-    path('category/breakfast-recipes/', views.breakfast, name='breakfast'),
-    path('category/lunch-recipes/', views.lunch, name='lunch'),
-    path('category/dinner-recipes/', views.dinner, name='dinner'),
-    path('category/dessert-recipes/', views.dessert, name='dessert'),
     path('add_recipe/', views.add_recipe, name='add_recipe'),
-
-    path('restricted/', views.restricted, name='restricted'),
     path('logout/', views.user_logout, name='logout'),
     path('userprofiles/<str:username>/', views.userprofile, name='userprofile'),
     path('userprofiles/<str:username>/add_recipe/', views.add_recipe, name='add_recipe'),
@@ -26,5 +20,6 @@ urlpatterns = [
          views.show_recipe, name='show_recipe'),
     path('goto/', views.goto_url, name='goto'),
     path('search/', views.search, name='search'),
+    path('get_recipe_data/<int:recipe_id>/', views.get_recipe_data, name='get_recipe_data'),
 
 ]
--- static/pdfconverter.js
+++ static/pdfconverter.js
@@ -1,7 +1,31 @@
-function myFunction(){
-    let htmlPDF = document.getElementById("html2pdf");
-    html2pdf(htmlPDF);
-}
+$j(document).ready(function() {
+    function fetchRecipeData(recipe_id) {
+        $j.ajax({
+            url: "/rmr/get_recipe_data/" + recipe_id + "/",
+            type: "GET",
+            success: function(data) {
+                // console.log(data);
 
+                var recipeTitle = data.title;
+                var recipeDescription = data.description;
+                var recipeInstructions = data.instructions;
+                var pdf = new jspdf.jsPDF();
+                pdf.text(30, 20, recipeTitle);
+                pdf.text(20, 30, recipeDescription);
+                pdf.text(20, 40, recipeInstructions);
+                pdf.save(recipeTitle + ".pdf");
+            },
+            error: function(jqXHR, textStatus, errorThrown) {
+                console.log(textStatus, errorThrown);
+            }
+        });
+    }
 
-        
+    const pdfButton = document.getElementById("pdf");
+    if (pdfButton) {
+        pdfButton.addEventListener("click", function() {
+            const recipeId = pdfButton.getAttribute("data-recipe-id");
+            fetchRecipeData(recipeId);
+        });
+    }
+});
